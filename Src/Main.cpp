4a834d08a221a228a5207e8a333245e26ac4710e
yozakuragekkouka <tsukitohikaritoyami@icloud.com>
2024-04-02 12:13:25 +0900

    First up
    はじめ

---
+++ Src/Main.cpp
@@ -0,0 +1,53 @@
+#include "DxLib.h"
+
+#define SCREEN_SIZE_X 960	//画面サイズ 横
+#define SCREEN_SIZE_Y 640	//画面サイズ 縦
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	//ウィンドウの状態を設定
+	ChangeWindowMode(true);
+
+	//初期化処理
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	//画面サイズを設定
+	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 32);
+
+	//描画するスクリーンを裏に設定
+	SetDrawScreen(DX_SCREEN_BACK);
+
+
+	/*最初に１度だけやる処理*/
+
+
+	//ゲームのメインループ
+	while (ProcessMessage() != -1)
+	{
+		//エスケープキーで終了
+		if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+
+		//画面を初期化
+		ClearDrawScreen();
+
+		// ↓ メインの処理
+
+
+		// 1ループ終了
+		ScreenFlip();
+	}
+
+	/*最後に１度だけやる処理*/
+
+
+	//DxLibの後処理
+	DxLib_End();
+
+	return 0;
+}
