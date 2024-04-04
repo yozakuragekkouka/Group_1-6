2c1509baabb8d0e72b028cd3a9a3f8e757d0a746
mikantmt <144639820+mikantmt@users.noreply.github.com>
2024-04-04 23:06:36 +0900

    sukoshihenkou

--- Src/Shimizu/JustStop.cpp
+++ Src/Shimizu/JustStop.cpp
@@ -2,12 +2,14 @@
 #include "../Nishiyama/Input/Input.h"
 #include "../Nishiyama/Shapes/Shapes.h"
 
+#define LINEY 400.0f
+
 void MiniGame::Init() {
 	handle = LoadGraph("Data/Image/");
 	IsPush = false;
 	X = 0.0f;
 	Y = 0.0f;
-	LineY = 0.0f;
+	LineY = LINEY;
 }
 
 void MiniGame::Play() {
@@ -47,12 +49,27 @@ float MiniGame::GetDistance(float rectY, float lineY) {
 }
 
 float MiniGame::GetPoint(float dist) {
-	int point;
+	int point = 0;
+	float i = LINEY / 100.0f;
 
 	if (dist == 0) {
 		point = 100;
 	}
-	
+	if (dist >= 0 && dist < 20 * i) {
+		point = 80;
+	}
+	if (dist >= 20 * i && dist < 40 * i) {
+		point = 60;
+	}
+	if (dist >= 40 * i && dist < 60 * i) {
+		point = 40;
+	}
+	if (dist >= 60 * i && dist < 80 * i) {
+		point = 20;
+	}
+	if (dist >= 80 * i && dist <= 100 * i) {
+		point = 10;
+	}
 	if (dist < 0) {
 		point = 0;
 	}
