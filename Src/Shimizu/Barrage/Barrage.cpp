f7d7153703c9372a110a0829b4a505b466285a49
mikantmt <144639820+mikantmt@users.noreply.github.com>
2024-04-09 04:08:07 +0900

    tuika

--- Src/Shimizu/Barrage/Barrage.cpp
+++ Src/Shimizu/Barrage/Barrage.cpp
@@ -1,111 +1,82 @@
 #include "Barrage.h"
+#include "../../Common.h"
 #include "../../Nishiyama/Input/Input.h"
-#include "../../Nishiyama/Shapes/Shapes.h"
 
 const int MAX_COUNT = 40;
 
+Barrage::~Barrage()
+{
+	back.RectFin();
+
+	DeleteGraph(Buf_Screen);
+
+	if (GetMouseDispFlag() == FALSE)
+	{
+		SetMouseDispFlag(TRUE);
+	}
+}
+
 void Barrage::Init() {
+	if (GetMouseDispFlag() == TRUE)
+	{
+		SetMouseDispFlag(FALSE);
+	}
+
 	Startlimit = 210.0f;//開始まで3秒+START表示の猶予(0.5秒)
 	count = MAX_COUNT;
 	time = 300.0f;	//5秒
-	handle = LoadGraph("Data/Image/");
 	click_flag = false;
+
+	isClear = false;
+	isFinish = false;
+
+	strcpy_s(toDo, "連打しろ！");
+
+	Buf_Screen = MakeScreen(SCREEN_SIZE_X, SCREEN_SIZE_Y, TRUE);
+
+	back.RectInit(LoadGraph(BARRAGE_BACK_PATH), VGet(0.0f, 0.0f, 0.0f), 1280, 720);
 }
 
-void Barrage::Play() {
-	Startlimit--;//スタートまでのカウントダウン
+void Barrage::Step() {
+	SetDrawScreen(Buf_Screen);
+	ClearDrawScreen();
+	SetDrawScreen(DX_SCREEN_BACK);
 
-	if (Startlimit <= 0) {
-		time--;
-		if (!isClear) {
-			if (IsClick()) {//指定のキーが押されるたびにカウントをマイナス
-				count--;
-			}
-		}
+	time--;
+	if (IsClick()) {//指定のキーが押されるたびにカウントをマイナス
+		count--;
 	}
 
+	if (count <= 0) {
+		isClear = true;
+	}
 
 	if (time <= 0.0f || count <= 0) {//タイムオーバー or カウントが0で終了
 		isFinish = true;
-		/* = GetPoint(count);*/
 	}
 }
 
 void Barrage::Draw() {
-	CountDown();
-	switch (GetPoint(count)) {
-	case 0:
-		//カウントフル
-		
-		break;
-	case 10:
-		//残り90%
+	SetDrawScreen(DX_SCREEN_BACK);
 
-		break;
-	case 20:
-		//残り80%
+	back.DrawRect();
 
-		break;
-	case 40:
-		//残り60%
+	DrawFormatString(SCREEN_SIZE_X / 2, SCREEN_SIZE_Y / 2, GetColor(0, 0, 0), "%d", count);
 
-		break;
-	case 60:
-		//残り40%
+	DrawString(SCREEN_SIZE_X / 2 - 50, 100, toDo, GetColor(0, 0, 0));
 
-		break;
-	case 80:
-		//残り20%
-
-		break;
-	case 100:
-		//残り0%
-
-		break;
-	}
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	SetDrawScreen(DX_SCREEN_BACK);
 }
 
 void Barrage::Fin() {
-	/*DeleteGraph();*/
-}
+	back.RectFin();
 
-int Barrage::GetPoint(int count) {
-	int point = 0;
-	if (count == MAX_COUNT) {
-		point = 0;
-	}
-	if (count < MAX_COUNT && count >= MAX_COUNT - 10) {
-		point = 20;
-	}
-	if (count < MAX_COUNT - 10 && count >= MAX_COUNT - 20) {
-		point = 40;
-	}
-	if (count < MAX_COUNT - 20 && count >= MAX_COUNT - 30) {
-		point = 60;
-	}
-	if (count < MAX_COUNT - 30 && count > MAX_COUNT - 40) {
-		point = 80;
-	}
-	if (count <= 0) {
-		point = 100;
-	}
+	DeleteGraph(Buf_Screen);
 
-	return point;
-}
-
-void Barrage::CountDown() {
-	//3,2,1のカウントダウン画像を配列で順番に表示
-	if (Startlimit <= 210.0f && Startlimit > 150.0f) {	//3
-		DrawFormatString(100, 100, GetColor(255, 255, 255), "3");
-	}
-	if (Startlimit <= 150.0f && Startlimit > 90.0f) {	//2
-		DrawFormatString(100, 100, GetColor(255, 255, 255), "2");
-	}
-	if (Startlimit <= 90.0f && Startlimit > 30.0f) {	//1
-		DrawFormatString(100, 100, GetColor(255, 255, 255), "1");
-	}
-	if (Startlimit <= 30.0f && Startlimit > 0.0f) {		//start
-		DrawFormatString(100, 100, GetColor(255, 255, 255), "start");
+	if (GetMouseDispFlag() == FALSE)
+	{
+		SetMouseDispFlag(TRUE);
 	}
 }
 
--- Src/Shimizu/Barrage/Barrage.h
+++ Src/Shimizu/Barrage/Barrage.h
@@ -1,17 +1,25 @@
 #pragma once
 #include "../../Nishiyama/MiniGame/MiniGameBase/MiniGameBase.h"
+#include "../../Nishiyama/Shapes/Shapes.h"
+
+constexpr char BARRAGE_BACK_PATH[128] = "Data/Oda/BackGround/WhiteBG.png";
 
 class Barrage : public MiniGameBase {
 private:
 	float Startlimit;//開始までのカウントダウン
 	int	  count;	 //連打回数
 	float time;		 //制限時間
-	int	  handle;
 	bool click_flag;
 	
+	int Buf_Screen;
+
+	Rect_Data back;
+
 public:
+	~Barrage();
+
 	void Init();
-	void Play();
+	void Step();
 	void Draw();
 	void Fin();
 
